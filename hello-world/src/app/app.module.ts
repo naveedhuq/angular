15714584fbc685040b44f99ffd77a742cec34489
naveed <naveedhuq@gmail.com>
2018-07-31 01:26:36 -0400

    upto service

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
